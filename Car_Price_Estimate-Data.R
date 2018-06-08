780da8ef8e9c6919327ecff84d083fe28ea104a0
Luay Dajani <luayd@hotmail.com>
2018-06-08 04:16:21 -0700

    updates to code

--- Car_Price_Estimate-Data.R
+++ Car_Price_Estimate-Data.R
@@ -9,4 +9,48 @@
 
 dfm_AutoData <- read.csv("data/autos.csv")
 
+# Exploratory Data
+
+## @knitr explore_data_plots
+
+# Basic Scatterplot Matrix
+pairs(~log(price)+yearOfRegistration+kilometer, 
+      data = dfm_AutoData,
+      main="Simple Scatterplot Matrix"
+)
+
+# Histogram of Price
+hist(dfm_AutoData$price, 
+     main = "Histogram of Price",
+     xlab = "Price"
+)
+
+# Histogram of log(Price)
+hist(log(dfm_AutoData$price), 
+     main = "Histogram of Price (log natural)",
+     xlab = "log_Price"
+     )
+
+# Histogram of Price
+hist(dfm_AutoData$yearOfRegistration, 
+     main = "Histogram of year of registration",
+     xlab = "Year"
+)
+
+# Histogram of log(Price)
+hist(log(dfm_AutoData$yearOfRegistration), 
+     main = "Histogram of year of registration (log natural)",
+     xlab = "log_Year"
+)
+
+# Histogram of KM
+hist(dfm_AutoData$kilometer, 
+     main = "Histogram of KM",
+     xlab = "KM"
+)
+
+
+
+
+
 
